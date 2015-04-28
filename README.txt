cf75fcd849d37320e69066011784724c41582c44
B Gray <strider_3029@yahoo.com>
2015-04-29 00:00:15 +1000

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+C# code sample
+
+CodeEval: Reverse Words
+URL: https://www.codeeval.com/open_challenges/8/
+
+Write a program which reverses the words in an input sentence.
---
+++ Reverse Words/ReverseWords.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ReverseWords
+{
+    class ReverseWords
+    {
+        static int Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (null == line) continue;
+
+                    string[] numberBlocks = line.Split(' ');
+
+                    string output = "";
+
+                    for (int index = numberBlocks.Length - 1; 0 <= index; --index)
+                    {
+                        output += numberBlocks[index] + " ";
+                    }
+
+                    output.Trim();
+
+                    if (output != "")
+                    {
+                        Console.WriteLine(output);
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
